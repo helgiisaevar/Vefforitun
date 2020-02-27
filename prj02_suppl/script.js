08869b0e60ef04177e8b9aae3d5a89c079c4aca3
ragnarr18 <ragnarr18@skel.ru.is>
2020-02-27 23:39:13 +0000

    validate params, and make the user input again if failed, u can input 'e', '+','-' and etc.

--- prj02_suppl/script.js
+++ prj02_suppl/script.js
@@ -30,6 +30,7 @@ function doAjax(rowCount, columCount, mineCount) {
 }
 
 function validateInput(rowCount, columCount, mineCount){
+    // just need a short validation check here
     document.getElementById("changeMe").innerHTML = "doing validation check"
     document.getElementById("changeMe1").innerHTML = "Validation checked out";
 }

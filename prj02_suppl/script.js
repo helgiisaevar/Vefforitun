7266b121e22553bb530cd5d92a99ffd481aacfb5
ragnarr18 <ragnarr18@skel.ru.is>
2020-02-27 23:38:01 +0000

    connection made with api

--- prj02_suppl/script.js
+++ prj02_suppl/script.js
@@ -1,17 +1,24 @@
-function doAjax() {
+function doAjax(rowCount, columCount, mineCount) {
+    // getting values by ID in html file
+    validateInput(rowCount, columCount, mineCount);
+    var rows = document.getElementById(rowCount).value;
+    var columns = parseInt(document.getElementById(columCount).value);
+    var mines = parseInt(document.getElementById(mineCount).value);
+    
+    var paramValue = {"rows":rows, "cols":columns,"mines": mines}
     //Prepare the parameter value for 'myParam'
-    var paramValue = "someValue";
+    //var paramValue = "/" + rows + "/" + columns + "/" + mines;
 
     //The URL to which we will send the request
     var url = 'https://veff213-minesweeper.herokuapp.com/api/v1/minesweeper';
 
     //Perform an AJAX POST request to the url, and set the param 'myParam' in the request body to paramValue
-    axios.post(url, { myParam: paramValue })
+    axios.post(url, paramValue)
         .then(function (response) {
             //When successful, print 'Success: ' and the received data
             document.getElementById("changeMe").innerHTML = response.data;
-            document.getElementById("changeMe1").innerHTML = "SJÉRÐU EKKI ARRAYI-IÐ....TO BAD";
             console.log("Success: ", response.data);
+            console.log(paramValue)
         })
         .catch(function (error) {
             //When unsuccessful, print the error.
@@ -20,6 +27,11 @@ function doAjax() {
         .then(function () {
             // This code is always executed, independent of whether the request succeeds or fails.
         });
+}
+
+function validateInput(rowCount, columCount, mineCount){
+    document.getElementById("changeMe").innerHTML = "doing validation check"
+    document.getElementById("changeMe1").innerHTML = "Validation checked out";
 }
 
 function printBoard(data) {

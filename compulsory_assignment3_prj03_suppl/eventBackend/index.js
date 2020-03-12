81909d417817e9a8fc608dd49efdf6114ba4766c
ragnarr18 <ragnarr18@skel.ru.is>
2020-03-12 14:08:02 +0000

    sdf

--- compulsory_assignment3_prj03_suppl/eventBackend/index.js
+++ compulsory_assignment3_prj03_suppl/eventBackend/index.js
@@ -1,23 +1,25 @@
 var express = require('express');
-var body_parser = require('body-parser');
+const assert = require('assert');
+var bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
-const port = 9000;
-
+const port = 3000;
+app.use(bodyParser.json());
 //Sample data for Assignment 3
-
+var bookings = [];
+var events = []
 //The following is an example of an array of two events. 
-var events = [
-    { id: 0, name: "The Whistlers", description: "Romania, 2019, 97 minutes", location: "Bio Paradís, Salur 1", capacity: 40, startDate: new Date(Date.UTC(2020, 02, 03, 22, 0)), endDate: new Date(Date.UTC(2020, 02, 03, 23, 45)), bookings: [0,1,2] },
-    { id: 1, name: "HarpFusion: Bach to the Future", description: "Harp ensemble", location: "Harpa, Hörpuhorn", capacity: 100, startDate: new Date(Date.UTC(2020, 02, 12, 15, 0)), endDate: new Date(Date.UTC(2020, 02, 12, 16, 0)), bookings: [] }
-];
+// var events = [
+//     { id: 0, name: "The Whistlers", description: "Romania, 2019, 97 minutes", location: "Bio Paradís, Salur 1", capacity: 40, startDate: new Date(Date.UTC(2020, 02, 03, 22, 0)), endDate: new Date(Date.UTC(2020, 02, 03, 23, 45)), bookings: [0,1,2] },
+//     { id: 1, name: "HarpFusion: Bach to the Future", description: "Harp ensemble", location: "Harpa, Hörpuhorn", capacity: 100, startDate: new Date(Date.UTC(2020, 02, 12, 15, 0)), endDate: new Date(Date.UTC(2020, 02, 12, 16, 0)), bookings: [] }
+// ];
 
 //The following is an example of an array of three bookings.
-var bookings = [
-    { id: 0, firstName: "John", lastName: "Doe", tel: "+3541234567", email: "", spots: 3},
-    { id: 1, firstName: "Jane", lastName: "Doe", tel: "", email: "jane@doe.doe", spots: 1},
-    { id: 2, firstName: "Meðaljón", lastName: "Jónsson", tel: "+3541111111", email: "mj@test.is", spots: 5}
-];
+// var bookings = [
+//     { id: 0, firstName: "John", lastName: "Doe", tel: "+3541234567", email: "", spots: 3},
+//     { id: 1, firstName: "Jane", lastName: "Doe", tel: "", email: "jane@doe.doe", spots: 1},
+//     { id: 2, firstName: "Meðaljón", lastName: "Jónsson", tel: "+3541111111", email: "mj@test.is", spots: 5}
+// ];
 
 app.get('/', (req, res) =>{
     res.status(200).send("hello there!")
@@ -26,6 +28,7 @@ app.get('/', (req, res) =>{
 app.get('/allEvents', (req, res) =>{
     res.status(200).send(events)
 })
+
 
 app.listen(port, () => {
     console.log("listening on port " + port)
